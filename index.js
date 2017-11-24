5b79c8ef48005e15d9ea14d754148bea32e039c5
Abhishek kalia <kabhi1792@gmail.com>
2017-11-24 19:50:53 +0530

    all package update

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import Root from './app/Root';
+import MainView from './app/components/MainView';
 
-AppRegistry.registerComponent('AwesomeApp', () => App);
+// import Login from "./app/auth/components/Login";
+
+AppRegistry.registerComponent('AwesomeApp', () => MainView);
